e7034606d59fb84041fb39e2f381e346090e59de
Matthew Evangelidis <matthew.evangelidis@students.mq.edu.au>
2020-11-22 21:30:07 +1100

    Initial commit for create-react-app
    needed to install typescript 4.0.5 due to current version incompatability

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div >
+      To paraphrase Anakain Skywalker, this is where the app begins
+    </div>
+  );
+}
+
+export default App;
